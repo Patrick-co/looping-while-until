54795d3478aaa07c30bcb5b696a3684763522a0e
Patrick <patricktaylec@gmail.com>
2021-02-05 19:39:54 -0500

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
+# current levitation force of 6
+# need levitation force to increase until 10 to levitate feather
+# puts phrase = "Wingardium Leviosa"
+
 def using_while
   levitation_force = 6
-	
-  #your code here
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
 
 
