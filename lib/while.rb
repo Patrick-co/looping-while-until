6c0e028cdfbc06d82db7ee824ac39a9eaa7b57d8
Patrick <patricktaylec@gmail.com>
2021-02-05 19:46:58 -0500

    commit tree d15830d8004cb688dcdbfdaa3fce10b5327f9d73

--- lib/while.rb
+++ lib/while.rb
@@ -3,10 +3,13 @@
 # puts phrase = "Wingardium Leviosa"
 
 def using_while
+  
   levitation_force = 6
+
   while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
+
   end
 end
 
